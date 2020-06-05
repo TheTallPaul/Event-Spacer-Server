17996f46b5094ade7ff5e6ea4e820042a75a4569
TheTallPaul <paul.lantow@gmail.com>
2020-06-04 17:25:34 -0700

    Created the maps and firestore repos

---
+++ internal/firestorerepo/db_repo.go
@@ -0,0 +1,49 @@
+package firebaserepo
+
+import (
+	"context"
+	"errors"
+	"log"
+	"time"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+
+	"google.golang.org/api/iterator"
+)
+
+// client is a Firestore client, reused between function invocations.
+var client *firestore.Client
+
+func init() {
+	// Use context.Background() because the app/client should persist across
+	// invocations.
+	ctx := context.Background()
+	config := &firebase.Config{ProjectID: projectID}
+
+	app, err := firebase.NewApp(ctx, config)
+	if err != nil {
+		log.Fatalf("firebase.NewApp: %v", err)
+	}
+
+	client, err = app.Firestore(ctx)
+	if err != nil {
+		log.Fatalf("app.Firestore: %v", err)
+	}
+}
+
+// FetchEvent returns a Firestore event document matching the provided ID
+func FetchEvent(ctx context.Context, eventID string) (Event, error) {
+	var event Event
+	doc, err := client.Doc("event/" + eventID).Get(ctx)
+	if err != nil {
+		return event, errors.New("Fetching event " + eventID +
+			" failed")
+	}
+	if err := doc.DataTo(&event); err != nil {
+		return event, errors.New("Failed to convert event json data " +
+			"to struct")
+	}
+
+	return event, nil
+}
---
+++ internal/firestorerepo/models.go
@@ -0,0 +1,18 @@
+package firebaserepo
+
+import (
+	"time"
+
+	"google.golang.org/genproto/googleapis/type/latlng"
+)
+
+// An Event is a struct conversion of a Firestore document for events
+type Ride struct {
+	ID            string           `firestore:"-"`
+	ClaimedSpots  []*latlng.LatLng `firestore:"claimed_spots"`
+	Expiration    time.Time        `firestore:"expiration"`
+	Name          string           `firestore:"name"`
+	NWBoundary    *latlng.LatLng   `firestore:"nw_boundary"`
+	SEBoundary    *latlng.LatLng   `firestore:"se_boundary"`
+	SpacingMeters float64          `firestore:"spacing_meters"`
+}
